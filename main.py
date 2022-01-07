7fb3514793d92dec33256545f595463bb312f043
PramyavincyGp <97107946+PramyavincyGp@users.noreply.github.com>
2022-01-07 12:58:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ from core import (
 
 REPO = """
 🤖 **Music Player**
-- Repo: [GitHub](https://github.com/AsmSafone/MusicPlayer)
+- Repo: [GitHub](https://github.com/PramyavincyGp/Music)
 - License: AGPL-3.0-or-later
 """
 
